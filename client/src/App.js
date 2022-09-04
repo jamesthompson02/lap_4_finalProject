b3a29f2159d05dc95fe602a7408d7aaace16b6ca
nouria chaouy <nouria.chaouy@gmail.com>
2022-09-04 15:48:07 +0100

    added pages and routes for pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import * as Pages from "./pages";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Pages.HomePage />} />
+      <Route path="/profile" element={<Pages.ProfilePage />} />
+      <Route path="/register" element={<Pages.RegisterPage />} />
+      <Route path="/logout" element={<Pages.LogoutPage />} />
+      <Route path="*" element={<Pages.NotFoundPage />} />
+    </Routes>
   );
 }
 
