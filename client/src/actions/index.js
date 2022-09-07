d55d1a0a92512fea19a41d4fa468376d7288c848
nouria chaouy <nouria.chaouy@gmail.com>
2022-09-07 09:51:21 +0100

    small modif

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -81,74 +81,71 @@ export const logoutUser = () => {
 };
 
 export const updateArtist = (artistName) => {
-    return {
-        type: "UPDATE ARTIST",
-        payload: artistName
-    }
-}
+  return {
+    type: "UPDATE ARTIST",
+    payload: artistName,
+  };
+};
 
 export const updateArtistSpotifyURI = (artistSpotifyIdentifier) => {
-    return {
-        type: "UPDATE ARTIST SPOTIFY URI",
-        payload: artistSpotifyIdentifier
-    }
-}
+  return {
+    type: "UPDATE ARTIST SPOTIFY URI",
+    payload: artistSpotifyIdentifier,
+  };
+};
 
 export const updateTrack = (trackName) => {
-    return {
-        type: "UPDATE TRACK",
-        payload: trackName
-    }
-}
+  return {
+    type: "UPDATE TRACK",
+    payload: trackName,
+  };
+};
 
 export const updateTrackSpotifyURI = (trackSpotifyIdentifier) => {
-    return {
-        type: "UPDATE TRACK SPOTIFY URI",
-        payload: trackSpotifyIdentifier
-    }
-}
+  return {
+    type: "UPDATE TRACK SPOTIFY URI",
+    payload: trackSpotifyIdentifier,
+  };
+};
 
 export const updateGenres = (genres) => {
-    return {
-        type: "UPDATE GENRES",
-        payload: genres
-    }
-}
+  return {
+    type: "UPDATE GENRES",
+    payload: genres,
+  };
+};
 
 export const updateFromLanguage = (language) => {
-    return {
-        type: "UPDATE FROM LANGUAGE",
-        payload: language
-    }
-}
+  return {
+    type: "UPDATE FROM LANGUAGE",
+    payload: language,
+  };
+};
 
 export const updateToLanguage = (language) => {
-    return {
-        type: "UPDATE TO LANGUAGE",
-        payload: language
-    }
-}
-
-
+  return {
+    type: "UPDATE TO LANGUAGE",
+    payload: language,
+  };
+};
 
 export const loading = (loadingStatus) => {
-    return {
-        type: "LOADING",
-        payload: loadingStatus
-    }
-}
+  return {
+    type: "LOADING",
+    payload: loadingStatus,
+  };
+};
 
 export const loaded = (loadingStatus) => {
-    return {
-        type: "LOADED",
-        payload: loadingStatus
-    }
-}
+  return {
+    type: "LOADED",
+    payload: loadingStatus,
+  };
+};
 
 export const errorMesage = (message) => {
-    return {
-        type: "SET ERROR",
-        payload: message
-    }
-}
-
+  return {
+    type: "SET ERROR",
+    payload: message,
+  };
+};
